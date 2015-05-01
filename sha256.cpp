c7fd913dd6d939a372bb0d21ecd8b99ecc7ab3c6
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-05-01 17:00:11 +0200

    Work on padding

--- sha256.cpp
+++ sha256.cpp
@@ -11,7 +11,7 @@ int l = 0;           // Message length in bits
 /**
  * Return the number of bytes needed to store the word.
  */
-const int needed_bytes(const WORD i)
+const int bytes_to_store(const WORD i)
 {
     int bytes = 1;
     if (i == 0)       return 0;
@@ -19,6 +19,20 @@ const int needed_bytes(const WORD i)
     if (i > 65535)    ++bytes;
     if (i > 16777215) ++bytes;
     return bytes;
+}
+
+/**
+ * Count the number of bits needed to represent the given word.
+ */
+const int bit_size(WORD w)
+{
+    int bit_size = 0;
+    while(w != 0)
+    {
+        ++bit_size;
+        w >>= 1;
+    }
+    return bit_size;
 }
 
 /**
@@ -43,7 +57,7 @@ const void store_message(const std::string &hex_str)
         WORD word = std::stoul(hex_str.substr(i * 8, hex_str.length() - i * 8),
                 nullptr, 16);
         M.push_back(word);
-        l += needed_bytes(word) * 8;
+        l += bytes_to_store(word) * 8;
     }
 }
 
@@ -51,26 +65,75 @@ const void store_message(const std::string &hex_str)
  * Calculate the required padding of the message.
  * Return the required padding.
  */
-const int calc_padding(const int l)
+const int calc_padding()
 {
     int k = 0;
     while ((l + 1 + k % 512) != 448) ++k;
     return k;
 }
 
+/**
+ * Pad the message M according to the specification.
+ */
+const void pad_message()
+{
+    int k = calc_padding();
+    int k_remainder = k % 32;
+    int bits;
+    WORD last_word;
+    // TODO Handle l > 512
+    if (l % 32 != 0)
+    {
+        last_word = M.back();
+        bits = bit_size(last_word);
+        // Append 1 before the least significant bit
+        last_word <<= 1;
+        last_word |= 0x1;
+
+        // Append k zeroes after the word
+        // TODO Assumes k > 32 - bits
+        for (WORD i = 0; i < (WORD) (32 - bits); ++i)
+        {
+            last_word <<= 1;
+            --k;
+        }
+        M.back() = last_word; // TODO Works?
+        for (WORD i = 0; i < (WORD) (k / 32); ++i) M.push_back(0);
+        // Check if one word is needed for trailing zeroes
+        if (k_remainder != 0) M.push_back(0);
+
+        // Finally append l in binary to the message
+        // TODO Why 64 bits in spec?
+        M.push_back(0); // Pad like l was a 64-bit number
+        M.push_back(0 | l); // Pad with the size in binary at the end
+    }
+}
+
 int main()
 {
     // Read each line as a hex string to be hashed
     for (std::string line; std::getline(std::cin, line);) {
+        // TODO Handle empty lines and the byte 0x00
         store_message(line);
-        std::cout << "M: ";
+
+        // DEBUG
+        std::cout << "M plain: ";
+        for (WORD i = 0; i < M.size(); ++i)
+            std::cout << M[i] << " ";
+        std::cout << std::endl;
+        std::cout << "l: " << l << std::endl;
+
+        // Check if message needs padding
+        if (l % 512 != 0) pad_message();
+
+        // DEBUG
+        std::cout << "M padded: ";
         for (WORD i = 0; i < M.size(); ++i)
             std::cout << M[i] << " ";
         std::cout << std::endl;
 
-        std::cout << "l: " << l << std::endl;
+        std::cout << "# words: " << M.size() << std::endl;
 
-        // Check if message needs padding
         M.clear(); // Reset message to hash a new one
     }
 }

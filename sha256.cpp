1847a2516dd1711952538ca6536ddcabc45885ee
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-05-01 12:02:16 +0200

    Work on reading input

--- sha256.cpp
+++ sha256.cpp
@@ -4,8 +4,14 @@
 
 typedef unsigned int WORD;
 
+std::vector<WORD> M; // Message to be hashed
 
-int calc_padding(int l)
+
+/**
+ * Calculate the required padding of the message.
+ * Return the required padding.
+ */
+const int calc_padding(const int l)
 {
     int k = 0;
     while ((l + 1 + k % 512) != 448) ++k;
@@ -15,7 +21,7 @@ int calc_padding(int l)
 /**
  * Convert the given hex string to a byte array.
  */
-void hex_str_to_byte_vector(const std::string &hex_str,
+const void hex_str_to_byte_vector(const std::string &hex_str,
         std::vector<char> &bytes)
 {
     std::stringstream converter;
@@ -31,20 +37,32 @@ void hex_str_to_byte_vector(const std::string &hex_str,
     }
 }
 
+/**
+ * Take the given hex string and store it in a word vector.
+ */
+const void store_message(const std::string &hex_str)
+{
+    // Pick out whole words from the the hex string
+    WORD i = 0;
+    for (; i < hex_str.length() / 8; ++i)
+    {
+        WORD word = std::stoul(hex_str.substr(i * 8, 8), nullptr, 16);
+        M.push_back(word);
+    }
+    // Pick out the last bytes which does not fit a word
+    WORD word = std::stoul(hex_str.substr(i * 8, hex_str.length() - i * 8),
+            nullptr, 16);
+    M.push_back(word);
+}
+
 int main()
 {
+    // Read each line as a hex string to be hashed
     for (std::string line; std::getline(std::cin, line);) {
-        /* std::vector<char> bytes; */
-
-        /* hex_str_to_byte_vector(line, bytes); */
-
-        /* for (std::vector<char>::iterator it = bytes.begin(), */
-        /*         end = bytes.end(); it != end; ++it) */
-        /* { */
-        /*     std::cout << *it << " "; */
-        /* } */
-
-        WORD word = std::stoul(line, nullptr, 16);
-        std::cout << word << std::endl;
+        store_message(line);
+        for (WORD i = 0; i < M.size(); ++i)
+            std::cout << M[i] << " ";
+        std::cout << std::endl;
+        M.clear(); // Reset message to hash a new one
     }
 }

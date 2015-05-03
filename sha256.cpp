f6ccb62914423d2b6e51648c5e44f5d8a94023b7
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-05-03 14:43:38 +0200

    Switch to storing input as bytes instead of words

--- sha256.cpp
+++ sha256.cpp
@@ -2,66 +2,29 @@
 #include <vector>
 #include <sstream>
 
+typedef unsigned char BYTE;
 typedef unsigned int WORD;
+typedef unsigned long long ll;
 
-std::vector<WORD> M; // Message to be hashed
-int l = 0;           // Message length in bits
+std::vector<BYTE> bytes; // Plain and padded message bytes
+std::vector<WORD> M;     // Message to be hashed
+ll l = 0;                // Message length in bits
 
-
-/**
- * Return the number of bytes needed to store the word.
- */
-const int bytes_to_store(const WORD i)
-{
-    int bytes = 0;
-    if (i > 0)        ++bytes;
-    if (i > 255)      ++bytes;
-    if (i > 65535)    ++bytes;
-    if (i > 16777215) ++bytes;
-    return bytes;
-}
 
 /**
- * Count the number of bits needed to represent the given word.
- */
-const int bit_size(WORD w)
-{
-    int bit_size = 0;
-    while(w != 0)
-    {
-        ++bit_size;
-        w >>= 1;
-    }
-    return bit_size;
-}
-
-/**
- * Take the given hex string and store it in a word vector.
+ * Take the given hex string and store the bytes in a global vector.
  * Also update the message length.
  */
-const void store_message(const std::string &hex_str)
+const void store_message_bytes(const std::string &hex_str)
 {
-    WORD str_length = hex_str.length();
-    // Pick out whole words from the the hex string
-    WORD i = 0;
-    std::cout << "message string length: " << str_length << std::endl;
-    for (; i < str_length / 8; ++i)
+    int str_length = hex_str.length();
+    std::cout << "Message string length: " << str_length << std::endl;
+    for (int i = 0, j = 0; i < str_length / 2; ++i, j += 2)
     {
-        WORD word = std::stoul(hex_str.substr(i * 8, 8), nullptr, 16);
-        std::cout << "store full word: " << word << std::endl;
-        M.push_back(word);
-        l += 32;
-    }
-
-    // Pick out the last bytes which does not fit a word
-    if (str_length % 8 != 0)
-    {
-        WORD word = std::stoul(hex_str.substr(i * 8, hex_str.length() - i * 8),
-                nullptr, 16);
-        std::cout << "store < full word: " << word << std::endl;
-        M.push_back(word);
-        std::cout << "with bytes to store: " << bytes_to_store(word) << std::endl;
-        l += bytes_to_store(word) * 8;
+        BYTE byte = std::stoi(hex_str.substr(j, 2), nullptr, 16);
+        std::cout << "Store byte: " << (WORD) byte << std::endl;
+        bytes.push_back(byte);
+        l += 8;
     }
 }
 
@@ -77,77 +40,43 @@ const int calc_padding()
 }
 
 /**
- * Pad the message M according to the specification.
+ * Pad the message bytes according to the specification.
  */
 const void pad_message()
 {
     int k = calc_padding();
-    int bits, zeroes;
-    WORD last_word;
+    int zeroes = 0; // DEBUG
 
     if (l % 512 == 0) return; // No padding necessary
 
     // TODO Handle l > 512
 
     std::cout << "k: " << k << std::endl;
-    // Add the zeroes of the first word
-    // Bitshift to make it correct when size not divisible by a word
-    if (l % 32 != 0)
+
+    // Append 1 followed by zeroes before the least significant bit,
+    // assuming full bytes as input
+    bytes.push_back(0x80);
+    k = k - 7;
+    zeroes += 7;
+
+    // Append the remaining zeroes of k
+    for (int i = 0; i < (k / 8); ++i)
     {
-        last_word = M.back();
-        std::cout << "last_word: " << last_word << std::endl;
-        bits = bit_size(last_word);
-        std::cout << "with bit size: " << bits << std::endl;
-        // Append 1 before the least significant bit
-        last_word <<= 1;
-        last_word |= 0x1;
-
-        zeroes = 0; // DEBUG
-
-        // Append k zeroes after the word by bitshifting the bytes into place
-        // TODO Assumes k > 32 - bits
-        for (WORD i = 0; i < (WORD) (32 - bits - 1); ++i)
-        {
-            last_word <<= 1;
-            --k;
-            ++zeroes;
-        }
-
-        std::cout << "last_word shifted: " << last_word << std::endl;
-
-        std::cout << "k after decr: " << k << std::endl;
-        M.back() = last_word;
-    }
-    // TODO Assume one additional word otherwise
-    else
-    {
-        zeroes = 31; // DEBUG
-
-        // Append 1 before the least significant bit
-        M.push_back(0xf0000000);
-        k = k - 31;
-    }
-
-    // Add the rest of the full words of 0's
-    for (WORD i = 0; i < (WORD) (k / 32); ++i)
-    {
-        M.push_back(0);
-        zeroes += 32;
-    }
-
-    // Check if one word is needed for trailing zeroes
-    if (k % 32 != 0)
-    {
-        M.push_back(0);
-        zeroes += 32;
+        bytes.push_back(0);
+        zeroes += 8;
     }
 
     std::cout << "zeroes: " << zeroes << std::endl;
 
-    // Finally append l in binary to the message
-    // TODO Why 64 bits in spec?
-    M.push_back(0); // Pad like l was a 64-bit number
-    M.push_back(0 | l); // Pad with the size in binary at the end
+    // l = 0x8040201008040201; // DEBUG
+
+    // Finally append the length in binary to the message as the least
+    // significant bits, assuming a 64-bit number
+    for (int i = 1; i < 9; ++i)
+    {
+        bytes.push_back(l >> (64 - i * 8));
+        std::cout << "Append byte: " << (WORD) (BYTE) (l >> (64 - i * 8)) << std::endl;
+    }
 }
 
 int main()
@@ -155,26 +84,27 @@ int main()
     // Read each line as a hex string to be hashed
     for (std::string line; std::getline(std::cin, line);) {
         // TODO Handle empty lines and the byte 0x00
-        store_message(line);
+        store_message_bytes(line);
 
-        std::cout << "M plain: ";
-        for (WORD i = 0; i < M.size(); ++i)
-            std::cout << M[i] << " ";
+        std::cout << "Read plain bytes: ";
+        for (WORD i = 0; i < bytes.size(); ++i)
+            std::cout << (WORD) bytes[i] << " ";
         std::cout << std::endl;
         std::cout << "l: " << l << std::endl;
 
-        std::cout << "Plain words: " << M.size() << std::endl;
+        std::cout << "Number of plain bytes: " << bytes.size() << std::endl;
 
         // Check if message needs padding
         if (l % 512 != 0) pad_message();
 
         std::cout << "Padded: ";
-        for (WORD i = 0; i < M.size(); ++i)
-            std::cout << M[i] << " ";
+        for (WORD i = 0; i < bytes.size(); ++i)
+            std::cout << (WORD) bytes[i] << " ";
         std::cout << std::endl;
 
-        std::cout << "Padded words: " << M.size() << std::endl;
+        std::cout << "Padded bytes: " << bytes.size() << std::endl;
 
-        M.clear(); // Reset message to hash a new one
+        bytes.clear(); // Reset message to hash a new one
+        // TODO Reset M as well
     }
 }
